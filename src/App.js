29ab0fad86d6adb76df297ebb1ed700901cba24e
McBreezy0 <jmgarr13@gmail.com>
2023-09-27 15:46:56 -0500

    name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/JacksonApp.js</code> and save to reloadGarr.
         </p>
         <a
           className="App-link"
